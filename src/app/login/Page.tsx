927c14b569d83b128c5c48fd96255ea0ad62ba32
hayaman3 <kevin.haya@yahoo.com>
2023-07-13 22:12:36 +0800

    added bare LoginPage

---
+++ src/app/login/Page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { FunctionComponent } from 'react';
+import LoginForm from './_components/Form';
+import Stack from '../../components/Layout/Stack';
+
+export type RegistrationPageProps = {
+	// No Props
+};
+
+const LoginPage: FunctionComponent<RegistrationPageProps> = props => {
+	const handleSubmit = (formData: any) => {
+		console.log(formData);
+	};
+
+	return (
+		<div className="mx-auto w-96">
+			<Stack className="space-y-4">
+				<h1>Registration</h1>
+				<LoginForm onSubmit={handleSubmit} />
+			</Stack>
+		</div>
+	);
+};
+
+export default LoginPage;
