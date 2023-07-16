e76901ab31dc0294e6b6733e612328fcc3eb876f
hayaman3 <kevin.haya@yahoo.com>
2023-07-16 21:22:56 +0800

    combine components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import LoginPage from './app/login/Page';
 import RegistrationPage from './app/registration/Page';
+import ToggleSwitch from './components/Buttons/Toggle';
 
 export default function App() {
 	return (
 		<main className="flex flex-col items-center justify-between">
+			<ToggleSwitch />
 			<RegistrationPage />
+			<LoginPage />
 		</main>
 	);
 }
